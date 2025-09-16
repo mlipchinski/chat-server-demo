d60aabf08524a5873da5dca968afa4246dd42ebb
mlipchinski <mlipchinski@gmail.com>
2025-09-16 13:20:15 +0300

    add mongo and rabbit locally via docker-compose and added db and rabbit password to .env to avoid commiting to git

--- user-service/src/config/config.ts
+++ user-service/src/config/config.ts
@@ -1,14 +1,33 @@
-import { config } from 'dotenv';
+import "dotenv/config";
 
-const configFile = `./.env`;
-config({ path: configFile, });
+const {
+    NODE_ENV,
+    PORT,
+    JWT_SECRET,
 
-const { MONGO_URI, PORT, JWT_SECRET, NODE_ENV, MESSAGE_BROKER_URL } = process.env;
+    MONGO_HOST,
+    MONGO_PORT,
+    MONGO_DB,
+    MONGO_INITDB_ROOT_USERNAME,
+    MONGO_INITDB_ROOT_PASSWORD,
+
+    RABBITMQ_HOST,
+    RABBITMQ_PORT,
+    RABBITMQ_DEFAULT_USER,
+    RABBITMQ_DEFAULT_PASS,
+    RABBITMQ_VHOST,
+} = process.env;
+
+// Build Mongo URI
+const MONGO_URI = `mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+
+// Build RabbitMQ URL
+const MESSAGE_BROKER_URL = `amqp://${RABBITMQ_DEFAULT_USER}:${RABBITMQ_DEFAULT_PASS}@${RABBITMQ_HOST}:${RABBITMQ_PORT}${RABBITMQ_VHOST}`;
 
 export default {
-    MONGO_URI,
-    PORT, 
-    JWT_SECRET,
     env: NODE_ENV,
+    PORT,
+    JWT_SECRET,
+    MONGO_URI,
     msgBrokerUrl: MESSAGE_BROKER_URL,
 };
