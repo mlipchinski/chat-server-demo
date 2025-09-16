2866a1793cc7284dda8a5b20335d5aa3d3c76b2c
mlipchinski <mlipchinski@gmail.com>
2025-09-16 09:44:14 +0300

    add User model

---
+++ user-service/src/database/models/UserModel.ts
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+import validator from "validator";
+
+export interface IUser extends Document {
+    name: string;
+    email: string;
+    password: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const UserSchema: Schema = new Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: [true, "Name must be provided"],
+            minlength: 3,
+        },
+        email: {
+            type: String,
+            trim: true,
+            unique: true,
+            required: true,
+            lowercase: true,
+            validate: [validator.isEmail, "Please provide a valid email."]
+        },
+        password: {
+            type: String,
+            trim: false,
+            required: [true, "Password must be provided"],
+            minlength: 8,
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+
+const User = mongoose.model<IUser>("User", UserSchema);
+
+export default User;
